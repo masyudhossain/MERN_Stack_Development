ec4577a478e70d00411bc41df220eb9c5f4597ce
Masyud Hossain <masyud.hossain2001@gmail.com>
2026-01-29 12:13:36 +0530

    Add HTML and JavaScript files for Exercise_Factorial, implementing factorial calculation

---
+++ JavaScript/Day7/script.js
@@ -0,0 +1,9 @@
+// let div = document.createElement("div");
+// div.innerHTML = "I have been inserted <b>by harry</b>"
+// div.setAttribute("class", "created");
+// document.querySelector(".container").before(div);
+// let cont = document.querySelector(".container");
+// cont.insertAdjacentHTML(
+//   "beforebegin",
+//   "<b> I am under the water. Please help me here too much raining....</b>"
+// );
